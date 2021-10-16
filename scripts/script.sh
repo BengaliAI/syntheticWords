8c807b1f4bd9dd7ba8723fed923e6c99a04fceb8
rezwanh001 <rezwanh001@gmail.com>
2021-10-17 04:07:50 +0600

    datagen

--- scripts/script.sh
+++ scripts/script.sh
@@ -12,12 +12,12 @@ bh_ds="${ds_path}bh/"
 bs_ds="${ds_path}bs/"
 #-----------------------------------bangla-----------------------------------------------
 #-----------------------------------natrual---------------------------------------------
-#python datasets/bangla_writing.py $bw_ref $ds_path
-#python datasets/boise_state.py $bs_ref $ds_path
-#python datasets/bn_htr.py $bh_ref $ds_path
+python datasets/bangla_writing.py $bw_ref $ds_path
 python datagen.py $bw_ds "bangla"
-python datagen.py $bh_ds "bangla"
+python datasets/boise_state.py $bs_ref $ds_path
 python datagen.py $bs_ds "bangla"
+python datasets/bn_htr.py $bh_ref $ds_path
+python datagen.py $bh_ds "bangla"
 #-----------------------------------natrual---------------------------------------------
 #-----------------------------------synthetic------------------------------------------
 python datagen_synth.py $data_dir "bangla" "printed" $ds_path --num_samples 1000000
