6de6d6730c99517825cae3f8bec3891dbd3e2685
silentassasin0111 <silentassasin0111@gmail.com>
2021-10-17 14:27:39 +0600

    iit-indic

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,27 +1,70 @@
 #!/bin/sh
-#base_path="/media/ansary/DriveData/Work/APSIS/datasets/GVU/"
 base_path="/home/apsisdev/ansary/DATASETS/GVU/"
 bw_ref="/home/apsisdev/ansary/DATASETS/RAW/bangla_writing/converted/converted/"
 bh_ref="/home/apsisdev/ansary/DATASETS/RAW/BN-HTR/"
 bs_ref="/home/apsisdev/ansary/DATASETS/RAW/BanglaC/README.txt"
+iit_path="/home/apsisdev/Rezwan/cvit_iiit-indic/"
+
+#iit_path="/media/ansary/DriveData/Work/APSIS/datasets/__raw__/bengal/iiit-indic/"
+#base_path="/media/ansary/DriveData/Work/APSIS/datasets/GVU/"
 #-----------------------------------------------------------------------------------------------
 data_dir="${base_path}data/"
 ds_path="${base_path}datasets/"
 bw_ds="${ds_path}bw/"
 bh_ds="${ds_path}bh/"
 bs_ds="${ds_path}bs/"
+
+iit_bn_ref="${iit_path}bn/vocab.txt"
+iit_bn_ds="${ds_path}iit.bn/"
+
+iit_gu_ref="${iit_path}gu/vocab.txt"
+iit_gu_ds="${ds_path}iit.gu/"
+
+iit_kn_ref="${iit_path}kn/vocab.txt"
+iit_kn_ds="${ds_path}iit.kn/"
+
+iit_ma_ref="${iit_path}ma/vocab.txt"
+iit_ma_ds="${ds_path}iit.ma/"
+
+iit_od_ref="${iit_path}od/vocab.txt"
+iit_od_ds="${ds_path}iit.od/"
+
+iit_pn_ref="${iit_path}pn/vocab.txt"
+iit_pn_ds="${ds_path}iit.pn/"
+
+iit_ta_ref="${iit_path}ta/vocab.txt"
+iit_ta_ds="${ds_path}iit.ta/"
+
+iit_ur_ref="${iit_path}ur/vocab.txt"
+iit_ur_ds="${ds_path}iit.ur/"
+
+
 #-----------------------------------bangla-----------------------------------------------
 #-----------------------------------natrual---------------------------------------------
-python datasets/bangla_writing.py $bw_ref $ds_path
-python datagen.py $bw_ds "bangla"
-python datasets/boise_state.py $bs_ref $ds_path
-python datagen.py $bs_ds "bangla"
-python datasets/bn_htr.py $bh_ref $ds_path
-python datagen.py $bh_ds "bangla"
+#python datasets/bangla_writing.py $bw_ref $ds_path
+#python datagen.py $bw_ds "bangla"
+#python datasets/boise_state.py $bs_ref $ds_path
+#python datagen.py $bs_ds "bangla"
+#python datasets/bn_htr.py $bh_ref $ds_path
+#python datagen.py $bh_ds "bangla"
+python datasets/iit_indic.py $iit_bn_ref $ds_path
+python datagen.py $iit_bn_ds "bangla"
 #-----------------------------------natrual---------------------------------------------
 #-----------------------------------synthetic------------------------------------------
-python datagen_synth.py $data_dir "bangla" "printed" $ds_path --num_samples 1000000
-python datagen_synth.py $data_dir "bangla" "handwritten" $ds_path --num_samples 500000
+#python datagen_synth.py $data_dir "bangla" "printed" $ds_path --num_samples 1000000
+#python datagen_synth.py $data_dir "bangla" "handwritten" $ds_path --num_samples 500000
 #-----------------------------------synthetic------------------------------------------
 #-----------------------------------bangla-----------------------------------------------
+
+#-----------------------------------indic-----------------------------------------------
+python datasets/iit_indic.py $iit_gu_ref $ds_path
+python datasets/iit_indic.py $iit_kn_ref $ds_path
+python datasets/iit_indic.py $iit_ma_ref $ds_path
+python datasets/iit_indic.py $iit_od_ref $ds_path
+python datasets/iit_indic.py $iit_pn_ref $ds_path
+python datasets/iit_indic.py $iit_ta_ref $ds_path
+python datasets/iit_indic.py $iit_ur_ref $ds_path
+#-----------------------------------indic-----------------------------------------------
+
+
 echo succeeded
